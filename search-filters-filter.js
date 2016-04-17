a4b1935bce096e7350d661c46796f247634c5f07
Dave Longley <dlongley@digitalbazaar.com>
2016-04-16 20:04:33 -0400

    Follow bedrock-angular conventions.

---
+++ search-filters-filter.js
@@ -0,0 +1,48 @@
+/*!
+ * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
+ */
+define([], function() {
+
+'use strict';
+
+// TODO: bikeshed name
+function register(module) {
+  module.filter('brSearchFilters', factory);
+}
+
+/* @ngInject */
+function factory() {
+  return filterSearchInput;
+}
+
+function filterSearchInput(input) {
+  var output = {};
+  if(!input) {
+    return output;
+  }
+  // Split spaces, but not spaces inside quotes
+  // Regex makes sure quotes are balanced. Don't ask
+  var terms = input.split(/ +(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
+  for(var i = 0; i < terms.length; i++) {
+    var component = terms[i].replace(/"/g, ""); // Remove quotes
+    if(component.indexOf(':') < 0) {
+      output = {error: 'Invalid search syntax near: ' + component};
+      break;
+    }
+    var property = component.substr(0, component.indexOf(':'));
+    if(property === 'is') {
+      property = 'filter';
+    }
+    var value = component.substr(component.indexOf(':') + 1);
+    if(!(property in output)) {
+      output[property] = [value];
+    } else {
+      output[property].push(value);
+    }
+  }
+  return output;
+}
+
+return register;
+
+});
